5f5210e9a5cdb15750dcb249544721a9a2dd2e4c
Alex Craven <w.alex.craven@gmail.com>
2016-10-27 18:26:35 -0500

    corrected error in knexfile.js that prevented connection to db

--- knexfile.js
+++ knexfile.js
@@ -30,10 +30,12 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'postgres://ptqkftcxusqolz:2M8koxuxoQk1b9WkPK_hd1f4hQ@ec2-54-243-52-115.compute-1.amazonaws.com:5432/d8o8mpl0b79muq',
-      user:     'ptqkftcxusqolz',
-      password: '2M8koxuxoQk1b9WkPK_hd1f4hQ'
-    },
+      user: 'ptqkftcxusqolz',
+      password: '2M8koxuxoQk1b9WkPK_hd1f4hQ',
+      host: 'ec2-54-243-52-115.compute-1.amazonaws.com',
+      database: 'd8o8mpl0b79muq',
+      ssl: true
+  },
     pool: {
       min: 2,
       max: 10
