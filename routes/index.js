15f4445681a56c391eb9703bf8b5eefc734739a5
Alex Craven <w.alex.craven@gmail.com>
2016-11-02 13:20:12 -0500

    connected mileage/new to db

--- routes/index.js
+++ routes/index.js
@@ -182,13 +182,11 @@ router.post('/api/mileage/new', function(req, res, next) {
       saveTrip.ClinicianId = newTrip.ClinicianId
       saveTrip.Date = newTrip.Date
       saveTrip.Mileage = saveTrip.rows[0].elements[0].distance.text
-      console.log('newTrip: ', newTrip);
-      console.log('saveTrip: ', saveTrip);
       res.json(saveTrip)
-      // client.query(`
-      //   INSERT INTO "Mileage" ("TripId", "ClinicianId", "Mileage", "Date")
-      //   VALUES ('${saveTrip.TripId}','${saveTrip.ClinicianId}','${saveTrip.Mileage}','${saveTrip.Date}')
-      // `)
+      client.query(`
+        INSERT INTO "Mileage" ("TripId", "ClinicianId", "Mileage", "Date")
+        VALUES ('${saveTrip.TripId}','${saveTrip.ClinicianId}','${saveTrip.Mileage}','${saveTrip.Date}')
+      `)
     } else {
       console.log(error);
     }
