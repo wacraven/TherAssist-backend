c9f294610623c55eda14f86555cc9d959aa2da1f
Alex Craven <w.alex.craven@gmail.com>
2016-11-27 13:19:38 -0500

    corrected typo in knex migration

--- migrations/20161127131240_dateupdate.js
+++ migrations/20161127131240_dateupdate.js
@@ -1,14 +1,14 @@
 
 exports.up = function(knex, Promise) {
   return knex.raw(
-    `ALTER TABLE "Patient" ALTER COLUMN "LastEvaluation" TYPE varchar(80);`
-    `ALTER TABLE "Patient" ALTER COLUMN "DateOfBirth" TYPE varchar(80);`
+    `ALTER TABLE "Patient" ALTER COLUMN "LastEvaluation" TYPE varchar(80);
+    ALTER TABLE "Patient" ALTER COLUMN "DateOfBirth" TYPE varchar(80);`
   )
 };
 
 exports.down = function(knex, Promise) {
   return knex.raw(
-    `ALTER TABLE "Patient" ALTER COLUMN "LastEvaluation" TYPE date;`
-    `ALTER TABLE "Patient" ALTER COLUMN "DateOfBirth" TYPE date;`
+    `ALTER TABLE "Patient" ALTER COLUMN "LastEvaluation" TYPE date;
+    ALTER TABLE "Patient" ALTER COLUMN "DateOfBirth" TYPE date;`
   )
 };
